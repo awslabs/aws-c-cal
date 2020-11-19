e1dec5f0c43f99bc5990cecba027729a9f9339ec
Justin Boswell <boswej@amazon.com>
2020-11-19 21:20:13 +0000

    stupid warning

--- source/unix/openssl_platform_init.c
+++ source/unix/openssl_platform_init.c
@@ -288,7 +288,8 @@ void aws_cal_platform_init(struct aws_allocator *allocator) {
 }
 
 void aws_cal_platform_clean_up(void) {
-    if (CRYPTO_get_locking_callback() == s_locking_fn) {
+    if (CRYPTO_get_locking_callback() != NULL &&
+        CRYPTO_get_locking_callback() == s_locking_fn) {
         CRYPTO_set_locking_callback(NULL);
         size_t lock_count = (size_t)CRYPTO_num_locks();
         for (size_t i = 0; i < lock_count; ++i) {
@@ -297,7 +298,8 @@ void aws_cal_platform_clean_up(void) {
         aws_mem_release(s_libcrypto_allocator, s_libcrypto_locks);
     }
 
-    if (CRYPTO_get_id_callback() == s_id_fn) {
+    if (CRYPTO_get_id_callback() != NULL &&
+        CRYPTO_get_id_callback() == s_id_fn) {
         CRYPTO_set_id_callback(NULL);
     }
 }

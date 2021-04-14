7a20fb31e88647591e9d763aff91110b4007c5cf
Itamar Levy-Or <ilevyor@amazon.com>
2021-04-14 14:52:03 -0400

    crc test changes

--- tests/crc_test.c
+++ tests/crc_test.c
@@ -8,13 +8,12 @@ static const uint32_t KNOWN_CRC32C_32_ZEROES = 0x8A9136AA;
 
 static const uint8_t DATA_32_VALUES[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15,
                                            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+static const uint32_t KNOWN_CRC32_32_VALUES = 0x91267E8A;
 static const uint32_t KNOWN_CRC32C_32_VALUES = 0x46DD794E;
 
 static const uint8_t TEST_VECTOR[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 static const uint32_t KNOWN_CRC32_TEST_VECTOR = 0xCBF43926;
 static const uint32_t KNOWN_CRC32C_TEST_VECTOR = 0xE3069283;
-
-static uint8_t *s_non_mem_aligned_vector;
 
 typedef struct aws_hash *(crc_fn)(struct aws_allocator *);
 
@@ -34,17 +33,17 @@ static int s_test_known_crc(
     ASSERT_HEX_EQUALS(expected, result, "%s(%s)", func_name, data_name);
 
     /* chain the crc computation so 2 calls each operate on about 1/2 of the buffer*/
-    const struct aws_byte_cursor input_fst_half = {
+    const struct aws_byte_cursor input_first_half = {
         .len = (int)(len / 2),
         .ptr = input->ptr,
     };
-    const struct aws_byte_cursor input_scnd_half = {
+    const struct aws_byte_cursor input_second_half = {
         .len = (int)(len - len / 2),
         .ptr = (input->ptr) + (len / 2),
     };
     struct aws_hash *crc1 = func(allocator);
-    aws_hash_update(crc1, &input_fst_half);
-    aws_hash_update(crc1, &input_scnd_half);
+    aws_hash_update(crc1, &input_first_half);
+    aws_hash_update(crc1, &input_second_half);
     uintptr_t result1 = (uintptr_t)crc1->impl;
     ASSERT_HEX_EQUALS(expected, result1, "chaining %s(%s)", func_name, data_name);
 
@@ -68,17 +67,23 @@ static int s_test_known_crc(
 static int s_test_known_crc32(const char *func_name, crc_fn *func, struct aws_allocator *allocator) {
     int res = 0;
     uint8_t *mut_DATA_32_ZEROS = (uint8_t *)DATA_32_ZEROS;
+    uint8_t *mut_DATA_32_VALUES = (uint8_t *)DATA_32_VALUES;
     uint8_t *mut_TEST_VECTOR = (uint8_t *)TEST_VECTOR;
     const struct aws_byte_cursor input = {
         .len = sizeof(DATA_32_ZEROS),
         .ptr = mut_DATA_32_ZEROS,
     };
     const struct aws_byte_cursor input1 = {
+        .len = sizeof(DATA_32_VALUES),
+        .ptr = mut_DATA_32_VALUES,
+    };
+    const struct aws_byte_cursor input2 = {
         .len = sizeof(TEST_VECTOR),
         .ptr = mut_TEST_VECTOR,
     };
     res |= s_test_known_crc(func_name, func, "DATA_32_ZEROS", &input, KNOWN_CRC32_32_ZEROES, allocator);
-    res |= s_test_known_crc(func_name, func, "TEST_VECTOR", &input1, KNOWN_CRC32_TEST_VECTOR, allocator);
+    res |= s_test_known_crc(func_name, func, "DATA_32_VALUES", &input1, KNOWN_CRC32_32_VALUES, allocator);
+    res |= s_test_known_crc(func_name, func, "TEST_VECTOR", &input2, KNOWN_CRC32_TEST_VECTOR, allocator);
     return res;
 }
 
@@ -105,9 +110,10 @@ static int s_test_known_crc32c(const char *func_name, crc_fn *func, struct aws_a
     res |= s_test_known_crc(func_name, func, "TEST_VECTOR", &input2, KNOWN_CRC32C_TEST_VECTOR, allocator);
 
     /*this tests three things, first it tests the case where we aren't 8-byte aligned*/
-    /*seconde, it tests that reads aren't performed before start of buffer*/
+    /*second, it tests that reads aren't performed before start of buffer*/
     /*third, it tests that writes aren't performed after the end of the buffer.*/
     /*if any of those things happen, then the checksum will be wrong and the assertion will fail */
+    uint8_t *s_non_mem_aligned_vector;
     s_non_mem_aligned_vector = malloc(sizeof(DATA_32_VALUES) + 6);
     memset(s_non_mem_aligned_vector, 1, sizeof(DATA_32_VALUES) + 6);
     memcpy(s_non_mem_aligned_vector + 3, DATA_32_VALUES, sizeof(DATA_32_VALUES));

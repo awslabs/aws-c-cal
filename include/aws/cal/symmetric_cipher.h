9eb2f50e24784c941604cf8303e72f3a58f0cffa
Jonathan M. Henson <jonathan.michael.henson@gmail.com>
2023-04-12 14:14:53 -0700

    OMG our linter is useless.

--- include/aws/cal/symmetric_cipher.h
+++ include/aws/cal/symmetric_cipher.h
@@ -30,9 +30,8 @@ typedef struct aws_symmetric_cipher *(aws_aes_gcm_256_new_fn)(
     const struct aws_byte_cursor *aad,
     const struct aws_byte_cursor *decryption_tag);
 
-typedef struct aws_symmetric_cipher *(aws_aes_keywrap_256_new_fn)(
-    struct aws_allocator *allocator,
-    const struct aws_byte_cursor *key);
+typedef struct aws_symmetric_cipher *(
+    aws_aes_keywrap_256_new_fn)(struct aws_allocator *allocator, const struct aws_byte_cursor *key);
 
 AWS_EXTERN_C_BEGIN
 

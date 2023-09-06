b6766ec7159aa085fd8b5af68e24030e041b6ff3
Michael Graeb <graebm@amazon.com>
2023-09-06 21:43:39 +0000

    Fix aws-c-cal-config.cmake

--- cmake/aws-c-cal-config.cmake
+++ cmake/aws-c-cal-config.cmake
@@ -2,6 +2,18 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(aws-c-common)
 
+if (NOT @BYO_CRYPTO@ AND NOT WIN32 AND NOT APPLE) # if NOT BYO_CRYPTO AND NOT WIN32 AND NOT APPLE
+    if (@USE_OPENSSL@ AND NOT ANDROID) # if USE_OPENSSL AND NOT ANDROID
+        # aws-c-cal has been built with a dependency on OpenSSL::Crypto,
+        # therefore consumers of this library have a dependency on OpenSSL and must have it found
+        find_dependency(OpenSSL REQUIRED)
+        find_dependency(Threads REQUIRED)
+    else()
+        list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
+        find_dependency(crypto)
+    endif()
+endif()
+
 macro(aws_load_targets type)
     include(${CMAKE_CURRENT_LIST_DIR}/${type}/@PROJECT_NAME@-targets.cmake)
 endmacro()
@@ -18,20 +30,5 @@ else()
         aws_load_targets(static)
     else()
         aws_load_targets(shared)
-    endif()
-endif()
-
-if (NOT BYO_CRYPTO AND NOT WIN32 AND NOT APPLE)
-    get_target_property(AWS_C_CAL_DEPS AWS::aws-c-cal INTERFACE_LINK_LIBRARIES)
-    # pre-cmake 3.3 IN_LIST search approach
-    list (FIND AWS_C_CAL_DEPS "OpenSSL::Crypto" _index)
-    if (${_index} GREATER -1) # if USE_OPENSSL AND NOT ANDROID
-        # aws-c-cal has been built with a dependency on OpenSSL::Crypto,
-        # therefore consumers of this library have a dependency on OpenSSL and must have it found
-        find_dependency(OpenSSL REQUIRED)
-        find_dependency(Threads REQUIRED)
-    else()
-        list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
-        find_dependency(crypto)
     endif()
 endif()

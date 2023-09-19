d665f610b65a548e06189d87899196aed3ae4a38
Dmitriy Musatkin <musatkd@amazon.com>
2023-09-19 00:52:30 -0700

    uneven len r s

--- tests/ecc_test.c
+++ tests/ecc_test.c
@@ -6,6 +6,8 @@
 #include <aws/cal/ecc.h>
 #include <aws/cal/hash.h>
 #include <aws/common/byte_buf.h>
+#include <aws/common/device_random.h>
+#include <aws/cal/private/der.h>
 #include <aws/common/encoding.h>
 #include <aws/common/string.h>
 #include <aws/testing/aws_test_harness.h>
@@ -929,3 +931,148 @@ static int s_ecdsa_p256_test_small_coordinate_verification(struct aws_allocator 
     return AWS_OP_SUCCESS;
 }
 AWS_TEST_CASE(ecdsa_p256_test_small_coordinate_verification, s_ecdsa_p256_test_small_coordinate_verification);
+
+static int s_ecdsa_p256_test_uneven_r_s(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
+
+    uint8_t asn1_encoded_full_key_raw[] = {
+        0x30, 0x77, 0x02, 0x01, 0x01, 0x04, 0x20, 0x99, 0x16, 0x2a, 0x5b, 0x4e, 0x63, 0x86, 0x4c, 0x5f, 0x8e, 0x37,
+        0xf7, 0x2b, 0xbd, 0x97, 0x1d, 0x5c, 0x68, 0x80, 0x18, 0xc3, 0x91, 0x0f, 0xb3, 0xc3, 0xf9, 0x3a, 0xc9, 0x7a,
+        0x4b, 0xa3, 0xf6, 0xa0, 0x0a, 0x06, 0x08, 0x2a, 0x86, 0x48, 0xce, 0x3d, 0x03, 0x01, 0x07, 0xa1, 0x44, 0x03,
+        0x42, 0x00, 0x04, 0xec, 0x6c, 0xd7, 0x4b, 0xdc, 0x33, 0xc2, 0x56, 0x32, 0xad, 0x52, 0x56, 0xac, 0xf5, 0xf0,
+        0xe6, 0x28, 0x99, 0x84, 0x83, 0xaf, 0x73, 0x6f, 0xfe, 0xd7, 0x83, 0x3b, 0x42, 0x81, 0x5d, 0x2e, 0xe0, 0xdb,
+        0xf6, 0xac, 0xa4, 0xc6, 0x16, 0x7e, 0x3e, 0xe0, 0xff, 0x7b, 0x43, 0xe8, 0xa1, 0x36, 0x50, 0x92, 0x83, 0x06,
+        0x94, 0xb3, 0xd4, 0x93, 0x06, 0xde, 0x63, 0x8a, 0xa1, 0x1c, 0x3f, 0xb2, 0x57, 0x0a,
+    };
+
+    struct aws_byte_cursor full_key_asn1 =
+        aws_byte_cursor_from_array(asn1_encoded_full_key_raw, sizeof(asn1_encoded_full_key_raw));
+
+    struct aws_ecc_key_pair *key_pair = aws_ecc_key_pair_new_from_asn1(allocator, &full_key_asn1);
+    ASSERT_NOT_NULL(key_pair);
+
+    uint8_t message[] = {
+        0xf1, 0x6e, 0xed, 0x63, 0x0f, 0xc2, 0xef, 0x0a, 0x74, 0x5f, 0x2f, 0x52, 0x37, 0x44, 0x05,
+        0x6d, 0x2e, 0x96, 0x56, 0x07, 0x56, 0xa2, 0x80, 0x88, 0xc0, 0x24, 0x12, 0x1e, 0x6d, 0x91,
+        0xa3, 0x5f,
+    };
+
+    uint8_t signature[] = {
+        0x30, 0x44, 0x02, 0x21, 0x00, 0xa3, 0xbc, 0xfe, 0xe8, 0xfa, 0x98, 0x6c, 0x2e, 0x62, 0x5b,
+        0x7b, 0x2e, 0xeb, 0xa6, 0x6d, 0xc0, 0x1a, 0x8e, 0x75, 0x15, 0x48, 0xfe, 0x9c, 0x2e, 0xa2,
+        0x3e, 0x84, 0xc3, 0xb2, 0xd5, 0x3f, 0x37, 0x02, 0x1f, 0x06, 0x01, 0x98, 0xaf, 0xf7, 0x18,
+        0x90, 0xee, 0x95, 0x37, 0x52, 0x4d, 0xb3, 0xdf, 0xd5, 0xd0, 0x9f, 0xc5, 0x7c, 0x43, 0xe2, 
+        0xd3, 0xf7, 0x76, 0x58, 0xf3, 0x77, 0x11, 0x48, 0x9a, 0x37
+    };
+
+    struct aws_byte_cursor message_cur = aws_byte_cursor_from_array(message, AWS_ARRAY_SIZE(message));
+    struct aws_byte_cursor sig_cur = aws_byte_cursor_from_array(signature, AWS_ARRAY_SIZE(signature));
+
+    ASSERT_SUCCESS(aws_ecc_key_pair_verify_signature(key_pair, &message_cur, &sig_cur));
+
+    aws_ecc_key_pair_release(key_pair);
+
+    return AWS_OP_SUCCESS;
+}
+AWS_TEST_CASE(ecdsa_p256_test_uneven_r_s, s_ecdsa_p256_test_uneven_r_s);
+
+static int s_ecdsa_p256_randomize(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
+
+    uint8_t asn1_encoded_full_key_raw[] = {
+        0x30, 0x77, 0x02, 0x01, 0x01, 0x04, 0x20, 0x99, 0x16, 0x2a, 0x5b, 0x4e, 0x63, 0x86, 0x4c, 0x5f, 0x8e, 0x37,
+        0xf7, 0x2b, 0xbd, 0x97, 0x1d, 0x5c, 0x68, 0x80, 0x18, 0xc3, 0x91, 0x0f, 0xb3, 0xc3, 0xf9, 0x3a, 0xc9, 0x7a,
+        0x4b, 0xa3, 0xf6, 0xa0, 0x0a, 0x06, 0x08, 0x2a, 0x86, 0x48, 0xce, 0x3d, 0x03, 0x01, 0x07, 0xa1, 0x44, 0x03,
+        0x42, 0x00, 0x04, 0xec, 0x6c, 0xd7, 0x4b, 0xdc, 0x33, 0xc2, 0x56, 0x32, 0xad, 0x52, 0x56, 0xac, 0xf5, 0xf0,
+        0xe6, 0x28, 0x99, 0x84, 0x83, 0xaf, 0x73, 0x6f, 0xfe, 0xd7, 0x83, 0x3b, 0x42, 0x81, 0x5d, 0x2e, 0xe0, 0xdb,
+        0xf6, 0xac, 0xa4, 0xc6, 0x16, 0x7e, 0x3e, 0xe0, 0xff, 0x7b, 0x43, 0xe8, 0xa1, 0x36, 0x50, 0x92, 0x83, 0x06,
+        0x94, 0xb3, 0xd4, 0x93, 0x06, 0xde, 0x63, 0x8a, 0xa1, 0x1c, 0x3f, 0xb2, 0x57, 0x0a,
+    };
+
+    struct aws_byte_cursor full_key_asn1 =
+        aws_byte_cursor_from_array(asn1_encoded_full_key_raw, sizeof(asn1_encoded_full_key_raw));
+
+    struct aws_ecc_key_pair *key_pair = aws_ecc_key_pair_new_from_asn1(allocator, &full_key_asn1);
+    ASSERT_NOT_NULL(key_pair);
+
+    size_t uneven = 0;
+
+    for (int i = 0; i < 10000; ++i) {
+        struct aws_byte_buf buf;
+        aws_byte_buf_init(&buf, allocator, 100);
+        ASSERT_SUCCESS(aws_device_random_buffer(&buf));
+
+        struct aws_byte_cursor message_input = aws_byte_cursor_from_buf(&buf);
+        uint8_t hash[AWS_SHA256_LEN];
+        AWS_ZERO_ARRAY(hash);
+        struct aws_byte_buf hash_value = aws_byte_buf_from_empty_array(hash, sizeof(hash));
+        aws_sha256_compute(allocator, &message_input, &hash_value, 0);
+
+        size_t signature_size = aws_ecc_key_pair_signature_length(key_pair);
+
+        struct aws_byte_buf signature_buf;
+        AWS_ZERO_STRUCT(signature_buf);
+        aws_byte_buf_init(&signature_buf, allocator, signature_size);
+
+        struct aws_byte_cursor hash_cur = aws_byte_cursor_from_buf(&hash_value);
+        ASSERT_SUCCESS(aws_ecc_key_pair_sign_message(key_pair, &hash_cur, &signature_buf));
+
+        struct aws_byte_cursor signature = aws_byte_cursor_from_buf(&signature_buf);
+        struct aws_der_decoder *decoder = aws_der_decoder_new(allocator, signature);
+        if (!decoder) {
+            return AWS_OP_ERR;
+        }
+
+        if (!aws_der_decoder_next(decoder) || aws_der_decoder_tlv_type(decoder) != AWS_DER_SEQUENCE) {
+            ASSERT_FALSE(true);
+        }
+
+        if (!aws_der_decoder_next(decoder) || aws_der_decoder_tlv_type(decoder) != AWS_DER_INTEGER) {
+            ASSERT_FALSE(true);
+        }
+
+        /* there will be two coordinates. They need to be concatenated together. */
+        struct aws_byte_cursor coordinate;
+        AWS_ZERO_STRUCT(coordinate);
+        if (aws_der_decoder_tlv_integer(decoder, &coordinate)) {
+            ASSERT_FALSE(true);
+        }
+        
+        size_t size_one = coordinate.len;
+
+        if (!aws_der_decoder_next(decoder) || aws_der_decoder_tlv_type(decoder) != AWS_DER_INTEGER) {
+            ASSERT_FALSE(true);
+        }
+
+        AWS_ZERO_STRUCT(coordinate);
+        if (aws_der_decoder_tlv_integer(decoder, &coordinate)) {
+            ASSERT_FALSE(true);
+        }
+        size_t size_two = coordinate.len;
+
+        if (size_one != 32 || size_two != 32 ) {
+            ++uneven;
+            //AWS_LOGF_DEBUG(0, "R S sizes %zu %zu", size_one, size_two);
+            struct aws_byte_buf hash_hex_buf;
+            aws_byte_buf_init(&hash_hex_buf, allocator, 1024);
+            ASSERT_SUCCESS(aws_hex_encode(&hash_cur, &hash_hex_buf));
+            struct aws_byte_buf sig_hex_buf;
+            aws_byte_buf_init(&sig_hex_buf, allocator, 1024);
+            ASSERT_SUCCESS(aws_hex_encode(&signature, &sig_hex_buf));
+            AWS_LOGF_DEBUG(0, "Failed hash " PRInSTR, AWS_BYTE_BUF_PRI(hash_hex_buf));
+            AWS_LOGF_DEBUG(0, "Failed signature " PRInSTR, AWS_BYTE_BUF_PRI(sig_hex_buf)); 
+            ASSERT_FALSE(true);
+        }
+        
+        aws_byte_buf_clean_up(&buf);
+        aws_byte_buf_clean_up(&signature_buf);
+        aws_der_decoder_destroy(decoder);
+    }
+
+    ASSERT_INT_EQUALS(uneven, 0);
+
+    aws_ecc_key_pair_release(key_pair);
+
+    return AWS_OP_SUCCESS;
+}
+AWS_TEST_CASE(ecdsa_p256_randomize, s_ecdsa_p256_randomize);

eef16517bcb41f49dd9cd396535d766ed3a42f44
DmitriyMusatkin <musatkd@amazon.com>
2024-10-30 16:36:27 -0700

    test print

--- tests/rsa_test.c
+++ tests/rsa_test.c
@@ -276,7 +276,14 @@ static int s_rsa_verify_signing_pkcs1_sha1(struct aws_allocator *allocator, void
         allocator, aws_byte_cursor_from_c_str(TEST_RSA_SIGNATURE_PKCS1_SHA1), &signature_buf));
     struct aws_byte_cursor signature_cur = aws_byte_cursor_from_buf(&signature_buf);
 
-    AWS_LOGF_DEBUG(0, "foo " PRInSTR " bar " PRInSTR, AWS_BYTE_CURSOR_PRI(signature_cur), AWS_BYTE_CURSOR_PRI(hash_cur));
+    size_t encoded_length = 0;
+    struct aws_byte_buf enc_buf;
+    ASSERT_SUCCESS(aws_base64_compute_encoded_len(hash_cur.len, &encoded_length));
+    ASSERT_SUCCESS(aws_byte_buf_init(&enc_buf, allocator, encoded_length));
+    ASSERT_SUCCESS(aws_base64_encode(&hash_cur, &enc_buf));
+    struct aws_byte_cursor enc_cur = aws_byte_cursor_from_buf(&enc_buf);
+
+    AWS_LOGF_DEBUG(0, "foo " PRInSTR " bar " PRInSTR, AWS_BYTE_CURSOR_PRI(enc_cur));
 
     ASSERT_SUCCESS(aws_rsa_key_pair_verify_signature(
         key_pair_public, AWS_CAL_RSA_SIGNATURE_PKCS1_5_SHA1, hash_cur, signature_cur));

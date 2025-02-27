13db423245d7dd3509498022725ceccf2b3fdf9f
DmitriyMusatkin <musatkd@amazon.com>
2025-02-27 11:07:19 -0800

    lc cleanup

--- source/darwin/commoncrypto_platform_init.c
+++ source/darwin/commoncrypto_platform_init.c
@@ -4,11 +4,28 @@
  */
 
 #include <aws/common/allocator.h>
+#if defined(AWS_USE_LIBCRYPTO_TO_SUPPORT_ED25519_EVERYWHERE) && defined(OPENSSL_IS_AWSLC)
+    #include <openssl/thread.h>
+#endif
 
 void aws_cal_platform_init(struct aws_allocator *allocator) {
     (void)allocator;
 }
 
-void aws_cal_platform_clean_up(void) {}
+#if defined(AWS_USE_LIBCRYPTO_TO_SUPPORT_ED25519_EVERYWHERE) && defined(OPENSSL_IS_AWSLC)
+void __attribute__((destructor)) s_cal_crypto_shutdown(void) {
+    AWSLC_thread_local_shutdown();
+}
+#endif
 
-void aws_cal_platform_thread_clean_up(void) {}
+void aws_cal_platform_clean_up(void) {
+#if defined(AWS_USE_LIBCRYPTO_TO_SUPPORT_ED25519_EVERYWHERE) && defined(OPENSSL_IS_AWSLC)
+    AWSLC_thread_local_clear();
+#endif
+}
+
+void aws_cal_platform_thread_clean_up(void) {
+#if defined(AWS_USE_LIBCRYPTO_TO_SUPPORT_ED25519_EVERYWHERE) && defined(OPENSSL_IS_AWSLC)
+    AWSLC_thread_local_clear();
+#endif
+}

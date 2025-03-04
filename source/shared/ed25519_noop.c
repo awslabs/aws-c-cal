d8fffb6752ad9986264fdeb0dec9db4d3a0e04dc
DmitriyMusatkin <musatkd@amazon.com>
2025-03-04 13:33:51 -0800

    remove static

--- source/shared/ed25519_noop.c
+++ source/shared/ed25519_noop.c
@@ -7,7 +7,7 @@
 
 struct aws_ed25519_key_pair_impl;
 
-static void aws_ed25519_key_pair_destroy_impl(struct aws_ed25519_key_pair_impl *key_pair) {
+void aws_ed25519_key_pair_destroy_impl(struct aws_ed25519_key_pair_impl *key_pair) {
     AWS_FATAL_ASSERT(key_pair == NULL);
     return;
 }
